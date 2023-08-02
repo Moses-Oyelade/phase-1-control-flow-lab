1b9dd57dc61a9baf20f958a80314f4aa876ff089
Moses <moses.oyelade@student.moringaschool.com>
2023-08-02 14:26:08 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    ride = "This one is on me!";
+  } else if (ride > 2000 && ride <= 2500){
+    ride = 'I will gladly take your thirty bucks.'
+  } else{
+    ride = 'No can do.'
+  }
+ return ride;   
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city == 'NYC' ? "Ok, sounds good." : "No go." ;
+ 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return 'Bye.'
+  }
 }
